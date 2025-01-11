4c967c710ef337daddd9fb8829050fd402869e7f
Jeremy Grosser <jeremy@synack.me>
2025-01-11 15:50:24 -0800

    repeated start

--- src/soft_i2c_slave.adb
+++ src/soft_i2c_slave.adb
@@ -136,6 +136,7 @@ package body Soft_I2C_Slave is
    begin
       if SCL = High and then Last_SDA = High and then SDA = Low then
          --  Start
+         Set_SDA (True); --  Release SDA in case this is a repeated start
          Current_State := Start;
          Bit_Count := 0;
          Data_Reg := 0;
