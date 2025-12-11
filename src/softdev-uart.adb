ff65cd87b37229e38d1b30f128cc2b4759c7ec62
Jeremy Grosser <jeremy@synack.me>
2025-12-11 11:36:13 -0800

    UART

---
+++ src/softdev-uart.adb
@@ -0,0 +1,105 @@
+package body Softdev.UART is
+
+   Oversample : constant := 2;
+   Start_Bit  : constant := 0;
+   Data_Bits  : constant := 8;
+   Stop_Bits  : constant := 1;
+
+   type Phase_Count is mod Oversample;
+   Phase : Phase_Count := 0;
+   TX_Phase : constant Phase_Count := 0;
+   RX_Phase : constant Phase_Count := 1;
+
+   TX_Buffer : UInt8 := 0; --  Data to send
+   TX_Bits   : UInt8 := 0; --  Bits remaining to send
+   TX_Count  : UInt8 := 0; --  Bit clock of TX
+
+   RX_Buffer : UInt8 := 0; --  Data received
+   RX_Bits   : UInt8 := 0; --  Bits received
+   RX_Count  : UInt8 := 0; --  Bit clock of RX
+
+   function Ready_To_Send
+      return Boolean
+   is (TX_Bits = 0);
+
+   function Data_Ready
+      return Boolean
+   is (RX_Bits >= 8);
+
+   procedure Write
+      (Data : UInt8)
+   is
+   begin
+      TX_Buffer := Data;
+      TX_Bits := 8;
+   end Write;
+
+   procedure Read
+      (Data : out UInt8)
+   is
+   begin
+      Data := RX_Buffer;
+      RX_Bits := 0;
+   end Read;
+
+   procedure Poll is
+      RX : Boolean;
+   begin
+      if Phase = TX_Phase then
+         if TX_Bits > 0 then
+            case TX_Count is
+               when 0 =>
+                  --  START
+                  Set_TXD (False);
+               when 1 .. Data_Bits =>
+                  Set_TXD ((TX_Buffer and 1) /= 0);
+                  TX_Buffer := Shift_Right (TX_Buffer, 1);
+                  TX_Bits := TX_Bits - 1;
+               when Data_Bits + 1 =>
+                  --  STOP
+                  Set_TXD (True);
+               when others =>
+                  null;
+            end case;
+
+            TX_Count := TX_Count + 1;
+            if TX_Count > (Data_Bits + Stop_Bits) then
+               TX_Count := 0;
+            end if;
+         else
+            Set_TXD (True);
+         end if;
+      end if;
+
+      if Phase = RX_Phase and then RX_Bits < 8 then
+         Get_RXD (RX);
+         case RX_Count is
+            when 0 =>
+               if not RX then
+                  --  START
+                  RX_Count := RX_Count + 1;
+               end if;
+            when 1 .. Data_Bits =>
+               RX_Buffer := Shift_Right (RX_Buffer, 1);
+               if RX then
+                  RX_Buffer := RX_Buffer or 16#80#;
+               end if;
+               RX_Bits := RX_Bits + 1;
+               RX_Count := RX_Count + 1;
+            when Data_Bits + 1 =>
+               if RX then
+                  RX_Count := RX_Count + 1;
+               end if;
+            when Data_Bits + 2 =>
+               if RX then
+                  RX_Count := 0;
+               end if;
+            when others =>
+               null;
+         end case;
+      end if;
+
+      Phase := Phase + 1;
+   end Poll;
+
+end Softdev.UART;
---
+++ src/softdev-uart.ads
@@ -0,0 +1,27 @@
+with HAL; use HAL;
+
+--  8N1 UART
+--  Call Poll every 52us for 9600 baud
+generic
+   with procedure Set_TXD (High : Boolean);
+   with procedure Get_RXD (High : out Boolean);
+package Softdev.UART is
+
+   function Ready_To_Send
+      return Boolean;
+
+   function Data_Ready
+      return Boolean;
+
+   procedure Write
+      (Data : UInt8)
+   with Pre => Ready_To_Send;
+
+   procedure Read
+      (Data : out UInt8)
+   with Pre => Data_Ready;
+
+   procedure Poll;
+   --  Called every ((1.0 / Baud_Rate) / 2.0) Seconds
+
+end Softdev.UART;
